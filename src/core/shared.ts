b04f5e05e0331843d4e2698377524be52997c7dd
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-02-18 17:31:08 +0800

    feat(pinia): optimize code, delete redundant variable

--- src/core/shared.ts
+++ src/core/shared.ts
@@ -2,8 +2,8 @@ import { name as APP_NAME } from '@/../manifest.json'
 import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 export const pinia = createPinia()
-export const persistedState = createPersistedState({
-    key: keyName => `${APP_NAME}.${keyName}`
-})
-
-pinia.use(persistedState)
+    .use(
+        createPersistedState({
+            key: keyName => `${APP_NAME}.${keyName}`
+        })
+    )
